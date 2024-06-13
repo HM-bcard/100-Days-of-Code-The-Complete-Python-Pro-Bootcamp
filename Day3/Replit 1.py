98399a3fed3f71792a5239dae371aac50b6ef827
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-06-13 02:30:25 +0200

    Create Replit 1.py

---
+++ Day3/Replit 1.py
@@ -0,0 +1,9 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height > 120:
+    print("Can ride")
+elif height < 120:
+    print("Can't ride")
+else:
+    print("Please input a correct value")
+
