88f917a0193787932d464f4bec53c033bce73388
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-06-13 02:48:57 +0200

    Update Replit 1.py

--- Day3/Replit 1.py
+++ Day3/Replit 1.py
@@ -1,9 +1,18 @@
 print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+height = int(input("What is your height in cm? \n"))
+age = int(input("What is your age?\n"))
 if height > 120:
-    print("Can ride")
+    
+    if age >= 18:
+            print("Can ride")
+    else:
+        print("can't ride due to age")
 elif height < 120:
     print("Can't ride")
 else:
     print("Please input a correct value")
 
+if height%2 == 0:
+    print("This is an even number")
+elif height % 2 != 0:
+    print("This is an odd number")
