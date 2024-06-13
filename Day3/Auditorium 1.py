56a6c5ca5b75fbdfda246ca4179050d8d7023b28
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-06-13 02:34:18 +0200

    Create Auditorium 1.py

---
+++ Day3/Auditorium 1.py
@@ -0,0 +1,11 @@
+# Which number do you want to check?
+number = int(input())
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+modulo = number%2
+if modulo == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+  
