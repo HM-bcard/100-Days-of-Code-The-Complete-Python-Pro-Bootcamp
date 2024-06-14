bdba8005e274aee88688cecf9f67e48becd36773
HM-bcard <111281060+HM-bcard@users.noreply.github.com>
2024-06-14 22:56:54 +0200

    Create HeadsorTails.py

---
+++ Day 4/HEadsorTails.py
@@ -0,0 +1,8 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+import random
+coin_toss = random.randint(0,1)
+if coin_toss == 0:
+  print("Tails")
+else:
+  print("Heads")
